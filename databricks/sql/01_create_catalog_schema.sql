447978702fe620b73f66cd7fe54f4892e9548b20
Deepayan Das <152376043+DeePZeeRa@users.noreply.github.com>
2026-04-27 19:05:37 +0530

    Delete databricks directory

--- databricks/sql/01_create_catalog_schema.sql
+++
@@ -1,3 +0,0 @@
--- Create catalog + schema
-CREATE CATALOG IF NOT EXISTS vf_health;
-CREATE SCHEMA IF NOT EXISTS vf_health.ghana;
--- databricks/sql/dev.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE bronze_raw_facilities ADD COLUMN source_file STRING;
-ALTER TABLE bronze_raw_facilities ADD COLUMN ingestion_batch_id STRING;
